35f04013b80f7e072401ffca339113f8b0fcaefe
kateryann <146440445+kateryann@users.noreply.github.com>
2024-03-16 02:10:09 +0000

    Update ENA_grab.sh

--- scripts/ENA_grab.sh
+++ scripts/ENA_grab.sh
@@ -1,7 +1,7 @@
 # Base command
 base_command="fastq-dl --outdir . --cpus 4 -a"
 
-# List of new sample identifiers
+# List of sample identifiers
 samples=("ERS2494002" "ERS2493884" "ERS2493826" "ERS2493842" "ERS2493883" "ERS2493754" "ERS2494023")
 
 # Loop through samples and download files
