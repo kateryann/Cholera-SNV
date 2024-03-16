bd768dfcd8648de73285f937c64d10a0d045a8b9
kateryann <146440445+kateryann@users.noreply.github.com>
2024-03-16 02:20:33 +0000

    Update cp_files.sh

--- scripts/cp_files.sh
+++ scripts/cp_files.sh
@@ -3,12 +3,6 @@
 # Target directory where .tsv files will be copied
 TARGET_DIR="/home/pc/Documents/08_03/ariba_reports"
 
-# Ensure the target directory exists
-if [[ ! -d "$TARGET_DIR" ]]; then
-  echo "Target directory does not exist: $TARGET_DIR"
-  exit 1
-fi
-
 # Find and copy all .tsv files from current and subdirectories to the target directory
 find . -type f -name "*.tsv" -exec cp {} "$TARGET_DIR" \;
 
